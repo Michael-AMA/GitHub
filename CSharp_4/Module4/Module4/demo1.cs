02d5af01626bbf01d62771249ffa0180813afecd
MichaelAMA <joebob60@hotmail.com>
2015-04-17 21:07:27 -0700

    Change name for from Module4 to Module4_demos.  Also consolidated all the demos into one program demo1.cs.

--- CSharp_4/Module4/Module4/demo1.cs
+++ CSharp_4/Module4/Module4/demo1.cs
@@ -6,23 +6,36 @@ using System.Threading.Tasks;
 
 namespace Module4
 {
+    struct Coffee2
+    {
+        public string Name { get; set; }
+        public string Bean { get; set; }
+        public string CountyOfOrigin { get; set; }
+        public int Strength { get; set; }
+        public string grind;
+
+    }
+    
     class Program
     {
         enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
 
         static void Main(string[] args)
         {
+            #region Single Dimensional Array
             // loop through single dimensional array
             Console.WriteLine("Single Dimensional Array");
-            int[] intArray = {5, 10, 1, 20, 25, 10};
-            for(int counter = 0; counter < intArray.Length; counter++)
+            int[] intArray = { 5, 10, 1, 20, 25, 10 };
+            for (int counter = 0; counter < intArray.Length; counter++)
             {
                 Console.WriteLine("{0}", intArray[counter]);
             }
+            #endregion
 
+            #region Two Dimensional Array
             Console.WriteLine("\nTwo Dimensional Array");
             // two dimensional array
-            int[,] twoDArray = { { 3, 2, 8, 9 }, { 4, 5, 6, 1 }, { 5, 6, 0, 5} };
+            int[,] twoDArray = { { 3, 2, 8, 9 }, { 4, 5, 6, 1 }, { 5, 6, 0, 5 } };
 
             for (int x = 0; x < twoDArray.GetLength(0); x++)
             {
@@ -32,13 +45,13 @@ namespace Module4
                     Console.WriteLine(value.ToString());
                 }
             }
+            #endregion
 
+            #region enum demo
             // Enums Arrays
             Console.WriteLine("");
             Console.WriteLine("Enums Array.");
 
-            // enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
-            #region enum demo
 
             int s = (int)Days.Thursday;
             int t = (int)Days.Friday;
@@ -47,7 +60,43 @@ namespace Module4
 
             #endregion
 
+            #region Structures #1
+            // Structures
+            Console.WriteLine("\nStructures");
+            
+            Coffee1 coffee1 = new Coffee1();
+            coffee1.Strength = 3;
+            coffee1.Bean = "Arabica";
+            coffee1.CountryOfOrigin = "Kenya";
+            Console.WriteLine("Bean:{0}\nStrength:{1}\nContry of Origin: {2}\n", coffee1.Bean, coffee1.Strength, coffee1.CountryOfOrigin);
+            #endregion 
 
+            #region using a struct
+
+            Coffee2 coffee = new Coffee2();
+            coffee.Name = "Fourth Coffee Quencher";
+            coffee.CountyOfOrigin = "Indonesia";
+            coffee.grind = "Fine";
+            coffee.Strength = 3;
+            Console.WriteLine("Name: {0}", coffee.Name);
+            Console.WriteLine("Country of Origin: {0}", coffee.CountyOfOrigin);
+            Console.WriteLine("Grind: {0}", coffee.grind);
+            Console.WriteLine("Strength: {0}", coffee.Strength);
+
+            #endregion
+
+
+
+
+
+
+        }
+
+        public struct Coffee1
+        {
+            public int Strength;
+            public string Bean;
+            public string CountryOfOrigin;
         }
     }
 }
--- CSharp_4/Module4/Structures/structs.cs
+++ CSharp_4/Module4/Structures/structs.cs
@@ -10,14 +10,13 @@ namespace Structures
     {
         static void Main(string[] args)
         {
-            
+
             Coffee coffee1 = new Coffee();
             coffee1.Strength = 3;
             coffee1.Bean = "Arabica";
             coffee1.CountryOfOrigin = "Kenya";
-
-            Console.WriteLine("\nStructures\nBeen:{0}", coffee1.Bean);
-            Console.Read();
+            //Coffee coffee1 = new Coffee(4, "Arabica", "Columbia");
+            Console.WriteLine("\nStructures\nBean:{0}", coffee1.Bean);
         }
 
         public struct Coffee
