d6eac2c8c49eeffbbe4c071e4a9d02aeeca81f40
joebob60 <joebob60@hotmail.com>
2015-05-04 19:00:07 -0700

    uploaded from myDevStation

---
+++ CSharp/Module5/Module5Demos/StaticClass_Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Anonymous Class
+            #region Anonymous Class
+            var anon = new { Name = "Mikie", Age = 31 };
+
+            Console.WriteLine(anon.Name + " " + anon.Age);
+            #endregion
+
+
+
+        }
+    }
+}
--- CSharp/Module5/Module5Demos/introDemo/Program.cs
+++ CSharp/Module5/Module5Demos/introDemo/Program.cs
@@ -10,6 +10,12 @@ namespace introDemo
     {
         static void Main(string[] args)
         {
+            DrinksMachine myMachine = new DrinksMachine("Kitchen", "Brand", "DM1000");
+
+            Console.WriteLine(myMachine.Location);
+            Console.WriteLine(myMachine.Make);            
+            Console.WriteLine(myMachine.Model);
+
         }
     }
 }
