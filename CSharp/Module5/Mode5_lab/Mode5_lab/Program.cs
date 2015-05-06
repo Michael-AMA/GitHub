100d5cc8d33eff2fd94891bd113c2004aec9f6e5
michaelAMA <joebob60@hotmail.com>
2015-05-05 17:46:40 -0700

    commit

---
+++ CSharp/Module5/Mode5_lab/Mode5_lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mode5_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentDetails myStudent = new StudentDetails();
+            myStudent.GetInfo();
+            myStudent.DisplayInfo();
+            TeacherDetails csTeacher = new TeacherDetails();
+            csTeacher.GetInfo();
+            csTeacher.DisplayInfo();
+        }
+    }
+}
---
+++ CSharp/Module5/Mode5_lab/Mode5_lab/TeacherDetails.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mode5_lab
+{
+    class TeacherDetails
+    {
+        private string _fName;
+
+        public string FName
+        {
+            get { return _fName; }
+            set { _fName = value; }
+        }
+        private string _lName;
+
+        public string LName
+        {
+            get { return _lName; }
+            set { _lName = value; }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("Teacher Information");
+            Console.WriteLine("First Name:");
+            FName = Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            LName = Console.ReadLine();
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Teacher: {0} {1}.", FName, LName);
+        }
+    }
+}
