850e7fe667318e629b353569e18a93d95331a596
michaelAMA <joebob60@hotmail.com>
2015-05-05 23:13:19 -0700

    added Module5_lab_v2 C# project

--- CSharp/Module5/Mode5_lab/Mode5_lab/Program.cs
+++ CSharp/Module5/Mode5_lab/Mode5_lab/Program.cs
@@ -10,12 +10,24 @@ namespace Mode5_lab
     {
         static void Main(string[] args)
         {
-            StudentDetails myStudent = new StudentDetails();
-            myStudent.GetInfo();
-            myStudent.DisplayInfo();
-            TeacherDetails csTeacher = new TeacherDetails();
-            csTeacher.GetInfo();
-            csTeacher.DisplayInfo();
+            ProgramClass.StudentInfo[] students = new ProgramClass.StudentInfo[3];
+            students[0] = new ProgramClass.StudentInfo();
+            students[1] = new ProgramClass.StudentInfo();
+            students[2] = new ProgramClass.StudentInfo();
+            students[0].Name = "student 1";
+            students[0].BDay = "4/20/84";
+            students[1].Name = "student 2";
+            students[1].BDay = "1/2/34";
+            students[2].Name = "student 3";
+            students[2].BDay = "8/29/91";
+
+            ProgramClass.Course[] ProgrammingWithCsharp = new ProgramClass.Course[3];
+            ProgrammingWithCsharp[0] = new ProgramClass.Course();
+            //ProgrammingWithCsharp[1] = new ProgramClass.Course();
+            //ProgrammingWithCsharp[2] = new ProgramClass.Course();
+            ProgrammingWithCsharp[0].CourseInfo = "Programming With C#";
+            ProgrammingWithCsharp[0].EnrolledStudents = students[0];
+            Console.WriteLine(ProgrammingWithCsharp[0].CourseInfo);
         }
     }
 }
---
+++ CSharp/Module5/Mode5_lab_v2/Mode5_lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mode5_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AllDetails.StudentInfo[] students = new AllDetails.StudentInfo[1];
+            students[0] = new AllDetails.StudentInfo();
+            students[0].StudentName = "Student 1";
+
+
+            Console.WriteLine("{0}", students[0].StudentName);
+        }
+    }
+}
