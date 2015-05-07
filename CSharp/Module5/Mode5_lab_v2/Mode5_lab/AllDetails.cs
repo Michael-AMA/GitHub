dcafab525f55b76f53ddb1038b3f4e436a67ccf0
michaelAMA <joebob60@hotmail.com>
2015-05-06 18:31:47 -0700

    Module5 lab, finished, testing still needed

--- CSharp/Module5/Mode5_lab_v2/Mode5_lab/AllDetails.cs
+++ CSharp/Module5/Mode5_lab_v2/Mode5_lab/AllDetails.cs
@@ -1,162 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Mode5_lab
-{
+//namespace Mode5_lab
+//{
 
-    class AllDetails
-    {
-        internal class StudentInfo
-        {
-            private string _studentName;
-
-            public string StudentName
-            {
-                get { return _studentName; }
-                set { _studentName = value; }
-            }
-
-            private string _birthday;
-
-            public string Birthday
-            {
-                get { return _birthday; }
-                set { _birthday = value; }
-            }
-        }
-
-        internal class StudentInfoLastName
-        {
-            private string _lastName;
-
-            public string LastName
-            {
-                get { return _lastName; }
-                set { _lastName = value; }
-            }
-        }
-
+//    class AllDetails
+//    {
         
+//        struct StudentInfo
+//        {
+//            private string _studentName;
 
-        internal class CourseDetails
-        {
-            private string _teacherName;
+//            public string StudentName
+//            {
+//                get { return _studentName; }
+//                set { _studentName = value; }
+//            }
 
-            public string TeacherName
-            {
-                get { return _teacherName; }
-                set { _teacherName = value; }
-            }
+//            private string _birthday;
+
+//            public string Birthday
+//            {
+//                get { return _birthday; }
+//                set { _birthday = value; }
+//            }
+//        }
         
-            private string _courseName;
+//        private string _teacherInfo;
 
-            public string CourseName
-            {
-                get { return _courseName; }
-                set { _courseName = value; }
-            }
-            private string _degreeName;
-
-            public string DegreeName
-            {
-                get { return _degreeName; }
-                set { _degreeName = value; }
-            }
-            private string _uProgram;
-
-            public string UProgram
-            {
-                get { return _uProgram; }
-                set { _uProgram = value; }
-            }
-        }
-//        #region StudentInfo Class
-//        internal class StudentInfo
+//        public string TeacherInfo
 //        {
-//            private string _name;
-
-//            public string Name
-//            {
-//                get { return _name; }
-//                set { _name = value; }
-//            }
-//            private string _bDay;
-
-//            public string BDay
-//            {
-//                get { return _bDay; }
-//                set { _bDay = value; }
-//            }
+//            get { return _teacherInfo; }
+//            set { _teacherInfo = value; }
 //        }
-//#endregion
-//        #region TeacherInfo class
-//        internal class TeacherInfo
+//        private string _uProgram;
+
+//        public string UProgram
 //        {
-//            private string _fName;
-
-//            public string FName
-//            {
-//                get { return _fName; }
-//                set { _fName = value; }
-//            }
-//            private string _lName;
-
-//            public string LName
-//            {
-//                get { return _lName; }
-//                set { _lName = value; }
-//            }
-
+//            get { return _uProgram; }
+//            set { _uProgram = value; }
 //        }
-//        #endregion
-//        #region UProgram Class
-//        internal class UProgram
-//        {
-//            private string _programInfo;
 
-//            public string ProgramInfo
-//            {
-//                get { return _programInfo; }
-//                set { _programInfo = value; }
-//            }
 
-//        }
-//        #endregion
-//        #region Degree Class
-//        internal class Degree
-//        {
-//            private string _degreeInfo;
-
-//            public string DegreeInfo
-//            {
-//                get { return _degreeInfo; }
-//                set { _degreeInfo = value; }
-//            }
-//        }
-//        #endregion
-//        #region Course Class
-//        internal class Course
-//        {
-//            private string _courseInfo;
-
-//            public string CourseInfo
-//            {
-//                get { return _courseInfo; }
-//                set { _courseInfo = value; }
-//            }
-
-//            private string _enrolledStudents;
-
-//            public string EnrolledStudents
-//            {
-//                get { return _enrolledStudents; }
-//                set { _enrolledStudents = value; }
-//            }
-//        }
-//        #endregion
-
-    }
-}
+//    }
+//}
--- CSharp/Module5/Mode5_lab_v2/Mode5_lab/Program.cs
+++ CSharp/Module5/Mode5_lab_v2/Mode5_lab/Program.cs
@@ -10,12 +10,24 @@ namespace Mode5_lab
     {
         static void Main(string[] args)
         {
-            AllDetails.StudentInfo[] students = new AllDetails.StudentInfo[1];
-            students[0] = new AllDetails.StudentInfo();
-            students[0].StudentName = "Student 1";
+            UProgram ComputerScience = new UProgram();
+            ComputerScience.ProgramName = "Information Technology";
+            UProgram.Degree BS = new UProgram.Degree();
+            BS.DegreeName = "Bachelor of Science";
+            UProgram.Degree.Course[] ProgrammingWithCSharp = new UProgram.Degree.Course[3];
+            ProgrammingWithCSharp[0].CourseName = "Programming with C#";
+            UProgram.Degree.Course.Student[] students = new UProgram.Degree.Course.Student[3];
+            students[0].StudentName = "student 1";
+            students[1].StudentName = "student 2";
+            students[2].StudentName = "student 3";
+            UProgram.Degree.Course.Teacher[] TeacherName = new UProgram.Degree.Course.Teacher[3];
+            TeacherName[0].TeacherName = "Pro. O'Brien";
+            UProgram.Degree.Course.Teacher.TeacherAssistant[] TAsName = new UProgram.Degree.Course.Teacher.TeacherAssistant[3];
+            TAsName[0].TAName = "Mike Jones";
 
-
-            Console.WriteLine("{0}", students[0].StudentName);
+            Console.WriteLine("The {0} program contains the {1} degree.", ComputerScience.ProgramName, BS.DegreeName);
+            Console.WriteLine("The {0} degree contains the course {1}.", BS.DegreeName, ProgrammingWithCSharp[0].CourseName);
+            Console.WriteLine("The {0} course contains {1} student(s).", ProgrammingWithCSharp[0].CourseName, students.Length);
         }
     }
 }
