3c55c5acc074f7536a2e6cd437c1ba3dd39b29f6
michaelAMA <joebob60@hotmail.com>
2015-05-06 22:19:39 -0700

    module 6

---
+++ CSharp/Module6/DemoProjects/DemoProjects/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjects
+{
+    // base class
+    abstract class Employee
+    {
+        public virtual void Login()
+        {
+            Console.WriteLine("Employee login");
+        }
+
+        public abstract void Hire();
+
+        #region class encapsulate
+        private string empNumber;
+        private string lastName;
+        private string address;
+        private string firstName;
+
+
+        public string EmpNumber
+        {
+            get { return empNumber; }
+            set { empNumber = value; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+#endregion
+        
+    }
+}
---
+++ CSharp/Module6/DemoProjects/DemoProjects/Managers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjects
+{
+    // inheritance from Employee class; sub class
+    class Managers : Employee
+    {
+        public override void Login()
+        {
+            Console.WriteLine("Manager Login");
+        }
+
+        public override void Hire()
+        {
+            Console.WriteLine("Hire someone");
+        }
+
+        private string department;
+
+        public string Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+    }
+}
